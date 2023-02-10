68f0846642cec82e794c874d8c3519041aa92792
Tomek <tomaszbielawski@gmail.com>
2023-02-10 21:58:57 +0100

    uruchomienie przeglądarek za pomocą if lub switch

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,7 +11,7 @@ public class SeleniumTest {
     @Test
 
     public void openGooglePage() {
-             //CHROME start
+        //CHROME start
 //        String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\chromedriver_win32\\chromedriver.exe";
 //        System.setProperty("webdriver.chrome.driver", path);
 //        WebDriver driver = new ChromeDriver();
@@ -20,13 +22,53 @@ public class SeleniumTest {
 //        WebDriver driver = new FirefoxDriver();
 //        driver.get("https://google.com");
         //IE start
-        String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\IEDriverServer_x64_4.8.0\\IEDriverServer.exe";
-        System.setProperty("webdriver.ie.driver", path);
-        WebDriver driver = new InternetExplorerDriver();
-        driver.get("https://google.com");
+//        String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\IEDriverServer_x64_4.8.0\\IEDriverServer.exe";
+//        System.setProperty("webdriver.ie.driver", path);
+//        WebDriver driver = new InternetExplorerDriver();
+//        driver.get("https://google.com");
 
+    WebDriver driver = getDriver("chrome","firefox");
+    driver.get("https://google.com");
+   //driver.findElement(By.id("label"));
+    }
+    public static void dupa(){
 
+    }
+    public WebDriver getDriver(String browser, String brow){
+
+        String chromePath = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\chromedriver_win32\\chromedriver.exe";
+
+        if((browser == "chrome") & (brow == "firefox")) {
+           // String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\chromedriver_win32\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", chromePath);
+            return new ChromeDriver();
+
+            //driver.get("https://google.com");
+        }else if(browser == "firefox"){
+            String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\geckodriver-v0.32.2-win64\\geckodriver.exe";
+            System.setProperty("webdriver.gecko.driver", path);
+            return new FirefoxDriver();
+        } else if (browser == "ie") {
+            String path = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\IEDriverServer_x64_4.8.0\\IEDriverServer.exe";
+             System.setProperty("webdriver.ie.driver", path);
+             return new InternetExplorerDriver();
+        }  else throw new InvalidArgumentException("Podaj prawidłową nazwę");
+
+        //przykład drugi
+//            switch (browser){
+//                case "chrome":
+//                    String chromePath = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\chromedriver_win32\\chromedriver.exe";
+//                    System.setProperty("webdriver.chrome.driver", chromePath);
+//                    return new ChromeDriver();
+//                case "firefox":
+//                    String firefoxPath = "C:\\Users\\Mennica\\Documents\\Automaty\\WebDriver\\geckodriver-v0.32.2-win64\\geckodriver.exe";
+//                    System.setProperty("webdriver.gecko.driver", firefoxPath);
+//                    return new FirefoxDriver();
+//                default:
+//                    throw new InvalidArgumentException("Bledna dana");
+//            }
 
 
     }
+
 }
