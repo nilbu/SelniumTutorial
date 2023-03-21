0723389fd5173b1f5316a45e47976385a76b0416
Tomek <tomaszbielawski@gmail.com>
2023-03-21 12:28:52 +0100

    połaczenie z bazą danych

--- src/test/java/pl/urbancard/SCurbancardTest.java
+++ src/test/java/pl/urbancard/SCurbancardTest.java
@@ -12,19 +12,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.security.Key;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class SCurbancardTest {
 
     @Test
-    public void logowanie () throws InterruptedException {
+    public void logowanie () throws InterruptedException, SQLException, ClassNotFoundException {
 
     WebDriverManager.chromedriver().setup();
     WebDriver driver = new ChromeDriver();
     String www = "http://sc-wroclaw.aws.mennica.net/";
-    driver.get(www);
+   // driver.get(www);
+      WebDriverWait wait = new WebDriverWait(driver,10);
   //  String defwindow = driver.getWindowHandle(); //zmienna na defaultowe okno
+
+        //ASERCJA Z DANYMI W BAZIE
+
+        String dbUrl = "jdbc:mysql://192.168.65.153:5432/oauth";
+        String username = "oauth";
+        String password = "NSwbvKTyJ2MH3gZBM06L";
+
+
 
     //logowanie
         By userNAme = By.cssSelector("#_username");
@@ -38,9 +48,9 @@ public class SCurbancardTest {
 
         //spr raportu
 
-       Thread.sleep(8000);
+       //Thread.sleep(8000);
 
-
+       wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#menu > li.nav-item.item-44.last.deeper.parent > a > span")));
        By raport = By.cssSelector("#menu > li.nav-item.item-44.last.deeper.parent > a > span");
        driver.findElement(raport).click();
         //raport sprzedaży
@@ -56,7 +66,9 @@ public class SCurbancardTest {
 
 
 
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\'sub-item-45\']/li[1]/a")));
 
         By raportSprz1 = By.xpath("//*[@id=\'sub-item-45\']/li[1]/a");
         driver.findElement(raportSprz1).click();
@@ -97,7 +109,16 @@ public class SCurbancardTest {
         By raportSprz4 = By.cssSelector("#report_form_dateRange_range_1");
         driver.findElement(raportSprz4).click();
 
-
+//
+//
+//        // Sprawdzenie, czy pole z podanym selektorem CSS zawiera oczekiwany tekst
+//        WebElement usernameField = driver.findElement(By.cssSelector("input[type='text']"));
+//        String actualUsername = usernameField.getText();
+//        if (expectedUsername.equals(actualUsername)) {
+//            System.out.println("Pole zawiera oczekiwany tekst.");
+//        } else {
+//            System.out.println("Pole nie zawiera oczekiwanego tekstu.");
+//        }
 
 
 
